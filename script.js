7d0e0a62af005710d0c522dcbc056f44b23ecd31
DaeHyup <02ydh@naver.com>
2020-11-15 15:47:13 +0900

    settings

---
+++ script.js
@@ -0,0 +1,38 @@
+const quizData = [
+    {
+        question: 'How old is Hyup?',
+        a: '10',
+        b: '19',
+        c: '26',
+        d: '110',
+        correct: 'b' 
+    }, {
+        question: 'What is the most used programming language in 2029?',
+        a: 'Java',
+        b: 'C',
+        c: 'Python',
+        d: 'JavaScript',
+        correct: 'd'
+    }, {
+        question: 'Who is the President of US?',
+        a: 'Hyup',
+        b: 'Joe Biden',
+        c: 'Ivan Saldano',
+        d: 'Mihai Andrei',
+        correct: 'b'
+    }, {
+        question: 'What does HTML stand for?',
+        a: 'Hypertext Markup Language',
+        b: 'Cascading Style Sheet',
+        c: 'Jason Object Notation',
+        d: 'Helicopters Terminals Motorboats Lamborginis',
+        correct: 'a'
+    }, {
+        question: 'What year was JavaScript launched?',
+        a: '1996',
+        b: '1995',
+        c: '1994',
+        d: 'none of the above',
+        correct: 'd'
+    }
+]
